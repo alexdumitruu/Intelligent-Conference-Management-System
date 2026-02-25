52b5d4545da573476b533fcd2afe2b0ec523f7c3
Alexandru David Dumitru <alex.dumitru.g@gmail.com>
2026-02-25 10:57:00 +0200

    added attributes and relationships for user and conference role entities

--- backend/src/conferences/entities/conference-role.entity.ts
+++ backend/src/conferences/entities/conference-role.entity.ts
@@ -1,3 +1,33 @@
-import { Table, Model } from "sequelize-typescript";
+import { Table, Model, ForeignKey, BelongsTo } from "sequelize-typescript";
+import { Column, DataType } from "sequelize-typescript";
+import { Conference } from "./conference.entity";
+import { User } from "../../users/entities/user.entity";
+
+export enum ConferenceRoleType {
+  AUTHOR = "AUTHOR",
+  REVIEWER = "REVIEWER",
+  CHAIR = "CHAIR",
+}
+
 @Table({ tableName: "conference_roles", underscored: true })
-export class ConferenceRole extends Model {}
+export class ConferenceRole extends Model {
+  // attributes with foreign keys
+  @ForeignKey(() => User)
+  @Column({ type: DataType.INTEGER, allowNull: false })
+  userId!: number;
+
+  @ForeignKey(() => Conference)
+  @Column({ type: DataType.INTEGER, allowNull: false })
+  conferenceId!: number;
+
+  // attributes
+  @Column({ type: DataType.ENUM(...Object.values(ConferenceRoleType)), allowNull: false })
+  roleType!: ConferenceRoleType;
+
+  // relationships
+  @BelongsTo(() => User)
+  user!: User;
+
+  @BelongsTo(() => Conference)
+  conference!: Conference;
+}
--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,3 +1,52 @@
-import { Table, Model } from "sequelize-typescript";
+import { Table, Model, HasMany, Column, DataType } from "sequelize-typescript";
+import { ConferenceRole } from "../../conferences/entities/conference-role.entity";
+import { PaperAuthor } from "../../papers/entities/paper-author.entity";
+import { Review } from "../../reviews/entities/review.entity";
+import { ReviewComment } from "../../reviews/entities/review-comment.entity";
+import { PaperHistory } from "../../papers/entities/paper-history.entity";
+import { Bid } from "../../matching/entities/bid.entity";
+import { Conflict } from "../../matching/entities/conflict.entity";
+
+
 @Table({ tableName: "users", underscored: true })
-export class User extends Model {}
+
+export class User extends Model {
+    //attributes
+    @Column({type: DataType.STRING, allowNull: false, unique: true})
+    email!: string;
+
+    @Column({type: DataType.STRING, allowNull: false})
+    passwordHash!: string;
+
+    @Column({type: DataType.STRING, allowNull: false})
+    firstName!: string;
+
+    @Column({type: DataType.STRING, allowNull: false})
+    lastName!: string;
+
+    @Column({type: DataType.STRING})
+    affiliation?: string;
+
+
+    //relationships
+    @HasMany(() => ConferenceRole)
+    conferenceRoles!: ConferenceRole[];
+
+    @HasMany(() => PaperAuthor)
+    paperAuthors!: PaperAuthor[];
+
+    @HasMany(() => Review)
+    reviews!: Review[];
+
+    @HasMany(() => ReviewComment)
+    reviewComments!: ReviewComment[];
+
+    @HasMany(() => PaperHistory)
+    paperHistories!: PaperHistory[];
+
+    @HasMany(() => Bid)
+    bids!: Bid[];
+
+    @HasMany(() => Conflict)
+    conflicts!: Conflict[];
+}
